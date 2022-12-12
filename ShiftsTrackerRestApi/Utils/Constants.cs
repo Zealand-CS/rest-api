18438341ec4a1571c968e8f3c3fdf87307c0e956
Patrik Hoferica <p.hoferica@outlook.com>
2022-12-12 09:45:34 +0100

    constants

---
+++ ShiftsTrackerRestApi/Utils/Constants.cs
@@ -0,0 +1,9 @@
+namespace ShiftsTrackerRestApi.Utils;
+
+public static class Constants
+{
+    public const int MIN_NAME_LENGTH = 3;
+    public const int MAX_NAME_LENGTH = 30;
+    public const int MAX_EMAIL_LENGTH = 50;
+    public const int MIN_EMPLOYEE_ID_LENGTH = 0;
+}
