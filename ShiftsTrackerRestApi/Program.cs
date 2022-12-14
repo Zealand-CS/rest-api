bdec91798f65fc67ffe8e631c62599afd3152d72
Martin Holoubek <marholoubek@gmail.com>
2022-12-14 21:54:36 +0100

    add cors

--- ShiftsTrackerRestApi/Program.cs
+++ ShiftsTrackerRestApi/Program.cs
@@ -2,6 +2,9 @@ using Microsoft.EntityFrameworkCore;
 using ShiftsTrackerRestApi;
 using ShiftsTrackerRestApi.Managers;
 
+const string allowAllPolicy = "AllowAll";
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +13,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: allowAllPolicy,
+        policy =>
+        {
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+        });
+});
 
 
 // Add DbContext
@@ -24,6 +36,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(allowAllPolicy);
 
 app.UseHttpsRedirection();
 
